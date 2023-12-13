267ce11a107484b7824f97bc2fc6e7d8715ba25a
Brandon Turner <bcturner@gmail.com>
2023-12-13 13:27:30 -0500

    Fixed Like hitbox

--- clucker/Components/LikeButton.swift
+++ clucker/Components/LikeButton.swift
@@ -12,20 +12,12 @@ struct LikeButton: View {
     @State var isSet: Bool
     
     var body: some View {
-        Button {
-            isSet.toggle()
-        } label: {
-            if(isSet){
-                Image("like.fill")
-                    .resizable()
-                    .frame(width: 32.0, height: 32.0)
+        Image(isSet ? "like.fill" : "like")
+            .resizable()
+            .frame(width: 32.0, height: 32.0)
+            .onTapGesture {
+                isSet.toggle()
             }
-            else{
-                Image("like")
-                    .resizable()
-                    .frame(width: 32.0, height: 32.0)
-            }
-        }
     }
 }
 
